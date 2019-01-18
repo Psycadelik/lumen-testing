3847cb2c5bbdb4560565239ee80d916419b0740c
Psycadelik <Francisadrian9@hotmail.com>
2019-01-18 15:12:40 +0300

    initial commit

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+    ];
+
+});
+
+
+$factory->define(App\Product::class, function (Faker\Generator $faker ){
+    return [
+        'product_name' => $faker->word,
+        'product_description' => $faker->text,
+    ];
+});
